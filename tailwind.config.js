4b18801b85eced3b179a95e275dbf9b2371b88ee
erfan <erfanghezi5050@gmail.com>
2024-04-19 15:44:18 +0330

    add fonts , container configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        "Samim" : "Samim" ,
+        "Yekan" : "Yekan"
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        lg: '1rem',
+        xl: '2rem',
+        '2xl': '4rem',
+      },
+    },
   },
   plugins: [],
 }
