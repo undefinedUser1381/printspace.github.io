74d12c4cde3eb8cbb5d01c9d2b413bcf24f85c42
erfan <erfanghezi5050@gmail.com>
2024-04-30 16:16:14 +0330

    add Shop component

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,12 @@
+import Breadcrumb from "../../components/Breadcrumb/Breadcrumb"
+
+export default function Shop() {
+  return (
+    <>
+        <Breadcrumb link="shop" title="فروشگاه"/>
+        <div className="container">
+         shop
+        </div>
+    </>
+  )
+}
