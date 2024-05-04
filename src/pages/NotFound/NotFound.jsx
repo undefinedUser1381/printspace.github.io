4943df161e97ef1adc215e34d42f81da84ec3bed
erfan <erfanghezi5050@gmail.com>
2024-05-04 16:18:31 +0330

    Add this component

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="w-full h-[20rem] mt-10  flex items-center justify-center flex-col bg-[#F8F8F8]">
+        <p className="font-Yekan text-center text-[4rem]">404</p>
+        <p className="font-Samim text-center">صفحه مورد نظر یافت نشد!</p>
+    </div>
+  )
+}
