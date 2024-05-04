3473e2ce0c01cc6fa1fcd364da9344f548ff376f
erfan <erfanghezi5050@gmail.com>
2024-05-04 16:16:58 +0330

    Change tepmplate of this component and update

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,13 +1,8 @@
-import { Link } from "react-router-dom"
-
-export default function Breadcrumb({title,link}) {
+export default function Breadcrumb({children}) {
   return (
-    <div className="bg-[#F5F5F5] flex flex-col h-[15rem] mt-5 p-10">
-        <h2 className="text-center text-[2rem] font-bold mt-5">{title}</h2>
-        <div className="flex items-center mx-auto justify-between mt-5 *:text-[#7E7E7E] *:font-Samim w-32 *:text-[1rem]">
-            <Link to={"/"}>خانه</Link>
-            <div>----</div>
-            <Link to={`/${link}`}>{title}</Link>
+    <div className="p-10">
+        <div className="flex items-center mx-auto justify-between *:text-[#7E7E7E] *:font-Samim w-full *:text-[1rem]">
+            {children}
         </div>
     </div>
   )
