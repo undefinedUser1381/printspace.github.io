729777fe43795ab6bcc86a05f4136f70616fbaef
erfan <erfanghezi5050@gmail.com>
2024-05-04 11:27:41 +0330

    Routes updated

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import Basket from "./pages/basket/Basket.jsx"
-import Cart from "./pages/cart/Cart.jsx"
+import MainProduct from "./pages/MainProduct/MainProduct.jsx"
 import Panel from "./pages/panel/Panel.jsx"
 import Home from "./pages/home/Home.jsx"
 import Shop from "./pages/shop/Shop.jsx"
@@ -8,9 +8,9 @@ let routes =
 [
      {path:"/" , element : <Home/>},
      {path:"/basket" , element : <Basket/>},
-     {path:"/cart" , element : <Cart/>},
-     {path:"/panel" , element : <Panel/>},
      {path:"/shop" , element : <Shop/>},
+     {path:"/MainProduct/:ID" , element : <MainProduct/>},
+     {path:"/panel" , element : <Panel/>},
 ]
 
 export default routes;
