50d0a1128e41fe87d963e3bee24180a14741082b
erfan <erfanghezi5050@gmail.com>
2024-05-04 11:30:13 +0330

    add custom hooks folder

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+  const [products, setProducts] = useState([]);
+  const [comments,setComments] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
+  return [products, comments];
+}
