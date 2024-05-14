c2332da5b837c6d05f1d5f02c326d0a86d55c49e
erfan <erfanghezi5050@gmail.com>
2024-05-14 10:35:47 +0330

    Developing register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export default function Register() {
+
+  // const [email,setEmail] = useState("");
+
   return (
      <section className="mt-20">
         <div className="container">
@@ -8,11 +13,11 @@ export default function Register() {
                  <div className="flex mt-5 shadow-md flex-col justify-start gap-5 border p-5">
                    <div className="flex flex-col gap-2">
                      <label htmlFor="" className="font-Samim text-[#777]">ایمیل خود را وارد کنید <span className="text-red-600">*</span></label>
-                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]" />
+                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]"/>
                    </div>
                    <div className="flex flex-col gap-2">
                      <label htmlFor="" className="font-Samim text-[#777]">رمز عبور <span className="text-red-600">*</span></label>
-                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]" />
+                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]"/>
                    </div>
                    <button className="bg-[#1B6F58] text-center text-white p-3 font-Samim rounded-md w-28 cursor-pointer">ورود</button>
                  </div>
@@ -22,19 +27,19 @@ export default function Register() {
                  <div className="flex flex-col shadow-md gap-5 border p-5 justify-start mt-5">
                     <div className="flex flex-col gap-2">
                      <label htmlFor="" className="font-Samim text-[#777]">نام<span className="text-red-600">*</span></label>
-                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]" />
+                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]"/>
                     </div>
                     <div className="flex flex-col gap-2">
                      <label htmlFor="" className="font-Samim text-[#777]">پسورد<span className="text-red-600">*</span></label>
-                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]" />
+                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]"/>
                     </div>
                     <div className="flex flex-col gap-2">
                      <label htmlFor="" className="font-Samim text-[#777]">ایمیل<span className="text-red-600">*</span></label>
-                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]" />
+                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]"/>
                     </div>
                     <div className="flex flex-col gap-2">
                      <label htmlFor="" className="font-Samim text-[#777]">نام کاربری<span className="text-red-600">*</span></label>
-                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]" />
+                     <input type="text" className="transition focus:border-[#1B6F58] w-[100%] p-2 outline-none border bg-[#F5F5F5]"/>
                     </div>
                     <button className="bg-[#1B6F58] text-center text-white p-3 font-Samim rounded-md w-28 cursor-pointer">ثبت نام</button>
                  </div>  
